93edf122156de238730bd17b65f59fe00e1938bc
Renat <durandnathan604@gmail.com>
2026-05-28 23:04:11 +0300

    style(TaskForm): restyle TaskForm component into a modal window

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -2,10 +2,15 @@ import { type FC, useState } from "react"
 import style from "./TaskForm.module.css"
 import { IconButton } from "@mui/material"
 import ControlPointIcon from '@mui/icons-material/ControlPoint'
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded'
 import { useAppDispatch } from "../../app/hooks"
 import { addTask } from "../../features/board/boardSlice"
 
-const TaskForm: FC = () => {
+type TaskFormProps = {
+  onClose: () => void
+}
+
+const TaskForm: FC<TaskFormProps> = ({onClose}) => {
 
   const [formData, setFormData] = useState({
     title: '',
@@ -29,7 +34,7 @@ const TaskForm: FC = () => {
 
   const dispatch = useAppDispatch()
 
-  const handleSubmit = (e: React.SubmitEvent) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     dispatch(addTask(
       {
@@ -38,47 +43,84 @@ const TaskForm: FC = () => {
         status: 'todo',
         createdAt: new Date().toLocaleDateString('en-US', options)
     }))
+    onClose()
   }
 
   return (
     <div className={style.taskFormContainer}>
-     <form 
-      className={style.taskFormBox}
-      onSubmit={handleSubmit}
-    >
-      <input 
-        className={style.taskFormInput} 
-        type='text' 
-        placeholder="Заголовок задачи"
-        name='title'
-        value={formData.title}
-        onChange={handleChange}
-      />
-      <textarea 
-        className={style.taskFormDescrp} 
-        name="description"
-        value={formData.description}
-        onChange={handleChange}
-      >
-      </textarea>
+      <div className={style.modalOverlay} onClick={onClose}>
 
-      <div className={style.taskFormBottom}>
-        <select 
-          name="priority"
-          value={formData.priority}
-          onChange={handleChange}
-        >
-          <option id="low">low</option>
-          <option id="medium">medium</option>
-          <option id="high">high</option>
-        </select>
+        <div className={style.modalContent} onClick={(e) => e.stopPropagation()}>
 
-        <IconButton type='submit'>
-          <ControlPointIcon color="info" />
-        </IconButton>
+          <div className={style.modalHeader}>
+            <h3>Create New Task</h3>
+            <IconButton type="button" 
+              className={style.closeBtn} 
+              onClick={onClose}
+              sx={{
+                padding: '0'
+              }}
+            >
+              <CancelRoundedIcon 
+                sx={{
+                  fill: '#ea2c2c',
+                  path: '#fff'
+                }}
+              />
+            </IconButton>
+          </div>
+
+          <form 
+            className={style.taskFormBox}
+            onSubmit={handleSubmit}
+          >
+            <input 
+              className={style.taskFormInput} 
+              type='text' 
+              placeholder="Task title..."
+              name='title'
+              value={formData.title}
+              onChange={handleChange}
+            />
+            <textarea 
+              className={style.taskFormDescrp} 
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Add description..."
+            >
+            </textarea>
+
+            <div className={style.taskFormBottom}>
+              <div className={style.selectWrapper}>
+                <label htmlFor="priority">Priority: </label>
+                <select 
+                  name="priority"
+                  value={formData.priority}
+                  onChange={handleChange}
+                  className={style.taskFormSelect}
+                >
+                  <option id="low">Low</option>
+                  <option id="medium">Medium</option>
+                  <option id="high">High</option>
+                </select>
+              </div>
+              <IconButton 
+                type='submit'
+                sx={{
+                  '&.MuiIconButton-root': {
+                    padding: 0
+                  }
+                }}
+              >
+                <ControlPointIcon sx={{ fontSize: '1.8rem', color: '#4ea8de' }} />
+              </IconButton>
+            </div>
+
+          </form>
+        </div>
+
       </div>
-      
-     </form>
     </div>
   )
 }
--- src/ui/KanbanBoard/KanbanBoard.tsx
+++ src/ui/KanbanBoard/KanbanBoard.tsx
@@ -1,7 +1,11 @@
-import type { FC } from "react"
+import  {type FC, useState } from "react"
 import { useAppSelector } from "../../app/hooks"
 import Column from "../Column/Column"
+import TaskForm from "../../components/TaskForm/TaskForm"
+import { IconButton } from "@mui/material"
+import AddIcon from '@mui/icons-material/Add'
 import style from "./KanbanBoard.module.css"
+
 
 const KanbanBoard: FC = () => {
 
@@ -9,14 +13,45 @@ const KanbanBoard: FC = () => {
   const toDoTasks = tasksStore.filter(t => t.status === 'todo')
   const inProgressTasks = tasksStore.filter(t => t.status === 'in-progress')
   const doneTasks = tasksStore.filter(t => t.status === 'done')
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   
   return (
     <section className={style.boardContainer}>
+    
+      <div className={style.boardAddTaskBox}>
+        <IconButton 
+          className="board__boardAddTaskBtn"
+          onClick={() => setIsModalOpen(true)}
+          sx={{
+            '&.MuiIconButton-root': {
+              border: '1px solid rgb(255, 204, 0)',
+              borderRadius: '8px',
+              padding: '6px 8px',
+            }
+          }}
+        >
+          <AddIcon 
+            className="board__addTaskBtnIcon"
+            sx={{
+              '&.MuiSvgIcon-root': {
+                fontSize: '1.1rem',
+                color: '#fff'
+              }
+            }}
+          />
+          <span style={{color: '#fff', fontSize: '1rem'}}>Add task</span>
+        </IconButton>
+      </div>
+
+
       <div className={style.boardGrid}>
         <Column title='Backlog' status='todo' tasks={toDoTasks}/>
         <Column title='In Progress' status='in-progress' tasks={inProgressTasks}/>
         <Column title='Done' status='done' tasks={doneTasks}/>
       </div>
+
+      {isModalOpen && <TaskForm onClose={() => setIsModalOpen(false)}/>}
     </section>
   )
 }
